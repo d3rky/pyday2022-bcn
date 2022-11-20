4ae770ee7b07427a64284c278cb06f475351764a
Pavel Lonkin <pavel.lonkin@ingrammicro.com>
2022-11-20 12:52:57 +0100

    Add intrumentation + provide comments

--- fuzz-simple-example/fuzz_simple_example/fuzz.py
+++ fuzz-simple-example/fuzz_simple_example/fuzz.py
@@ -2,6 +2,9 @@ import atheris
 import sys
 
 
+# instrument the function to allow Atheris to track the coverage and provide more effective
+# strategy to generate and mutate input data bytes
+@atheris.instrument_func
 def check_permission(permission: str) -> bool:
     if permission == 'fuzzing_is_available':
         return False
@@ -13,6 +16,7 @@ def check_permission(permission: str) -> bool:
     return True
 
 
+@atheris.instrument_func
 def do_calc(permission: str) -> int:
     is_available = check_permission(permission)
 
@@ -26,13 +30,18 @@ def run_fuzzing(data: bytes):
     """
     Put here your fuzzing code
     """
+    # create the FuzzedDataProvider to interpretate the input sample bytes to
+    # something meaningfull
     dp = atheris.FuzzedDataProvider(data)
 
-    length = dp.ConsumeIntInRange(0, 15)
+    length = dp.ConsumeIntInRange(0, 32)
+    # generate the unicode sample. It only guarantees, that generated unicode will be
+    # up to provided length
     permission = dp.ConsumeUnicodeNoSurrogates(length)
 
     do_calc(permission)
 
 
+# setup and run Atheris Fuzzer
 atheris.Setup(sys.argv, run_fuzzing)
 atheris.Fuzz()
